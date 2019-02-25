9bbd9d24ad4747d02a52bdb4c39f92ca7f094e38
Kun1605 <1321366271@qq.com>
2019-02-25 22:51:46 +0800

    auto commit

--- commit.sh
+++ commit.sh
@@ -1,4 +1,19 @@
-#！bin/bash
+#!/bin/bash
+
+echo "git add ."
 git add .
-git commit -m '通过脚本自动提交的'
-git push origin master 
+
+echo "git commit"
+echo "请输入commit的注释信息:"
+comment="commit new code"
+read comment
+git commit -m "$comment"
+
+echo "git fetch origin master"
+git fetch origin master
+
+echo "git merge origin/master"
+git merge origin/master
+
+echo "git push origin master:master"
+git push origin master:master
